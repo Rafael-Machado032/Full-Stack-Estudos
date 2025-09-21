75bcba3273dcfbd6ebb17b7f21550234b9386306
Rafael-Machado032 <api.rafaelmachado@gmail.com>
2025-09-21 13:02:35 -0300

    Reogarnizar

---
+++ JavaScript Estudos/Projeto JavaScript 3 Modulo/bancoContatos.js
@@ -0,0 +1,2 @@
+let contatos=[] //Array onde vai ser armazenado os contatos
+export {contatos} //Esporta para contatos
---
+++ TypeScript Estudos/build/basico.js
@@ -0,0 +1,5 @@
+"use strict";
+const nome = "Rafael";
+const idade = 30;
+const cidade = "Guaíba";
+console.log(`Meu nome é ${nome}, tenho ${idade} anos e moro em ${cidade}.`);
