52a428723bf608466ea600b7eadf7aca4ff7eb27
Rafael-Machado032 <rafaelmfds@gmail.com>
2025-12-13 11:44:45 -0300

    Problema deo grafico

---
+++ Analise Biticoin/frontend/app/analise/page.tsx
@@ -0,0 +1,11 @@
+import MyChart from './MyChart';
+
+export default function HomePage() {
+    return (
+        <div>
+            <h1>Visualização de Dados</h1>
+            <MyChart />
+        </div>
+    );
+}
+
