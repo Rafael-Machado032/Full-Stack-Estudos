9189ee2928166aad1f13b5174498c61bca5bdb7b
Rafael-Machado032 <api.rafaelmachado@gmail.com>
2026-01-13 23:27:39 -0300

    a

--- Python Estudos/Graficos/Password.py
+++ Python Estudos/Graficos/Password.py
@@ -1,7 +1,19 @@
 from tkinter import *
 
+def impSenha():
+    print(f"Senha digitada "+ p_senha.get())
+
 app = Tk()
 app.title("Curso Python")
 app.geometry("500x400")
+
+vsenha=StringVar()
+
+p_senha=Entry(app,textvariable=vsenha,show="*") #show Insere o valor para esconder o caracter
+p_senha.pack()
+
+btn_mostrarSenha=Button(app,text="Imprime Senha",command=impSenha)
+btn_mostrarSenha.pack()
+
 
 app.mainloop()
